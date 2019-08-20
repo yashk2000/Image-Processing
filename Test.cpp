8bf4a91fc957d2aec86f23a40081f19cad2e4591
Yash Khare <yashsja@gmail.com>
2019-08-20 17:16:24 +0530

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,20 @@
+#include <opencv2/opencv.hpp>
+
+int main(int args, char** argv) {
+    
+    cv::namedWindow("Input Image", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("Output Image", cv::WINDOW_AUTOSIZE);
+    
+    cv::Mat input, img_gry, img_pyr, img_pyr2, output;
+    
+    input = cv::imread(argv[1]);
+    cv::imshow("Input Image", input);
+    
+    cv::cvtColor( input, img_gry, cv::COLOR_BGR2GRAY );
+    cv::pyrDown( img_gry, img_pyr );
+    cv::Canny( img_pyr, output, 10, 100, 3, true );
+
+    cv::imshow("Output Image", output);
+    
+    cv::waitKey(0);
+}
