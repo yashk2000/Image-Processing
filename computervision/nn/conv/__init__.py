8183cf06a999ed931e088d337f05b5c258ff315d
yashk2000 <yashsja@gmail.com>
2019-12-24 02:31:24 +0530

    Full GoogLeNet

--- computervision/nn/conv/__init__.py
+++ computervision/nn/conv/__init__.py
@@ -4,3 +4,5 @@ from .minivggnet import MiniVGGNet
 from .fcheadnet import FCHeadNet
 from .alexnet import AlexNet
 from .minigooglenet import MiniGoogLeNet
+from .deepergooglenet import DeeperGoogLeNet
+from .googlenet2 import GoogLeNet2
